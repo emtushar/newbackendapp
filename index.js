53187abcab38d31dbbc7580823454eec36f7cafe
emtushar <72194068+emtushar@users.noreply.github.com>
2024-03-08 13:16:41 +0000

    added some more routes

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-require('dotenv').config()
+// require('dotenv').config()
 import 'dotenv/config'
 // console.log(process.env.PORT)
 
 // const express = require("express");
 import express from "express";
 const app = express();
-app.get("/",(req,res)=>{
-res.send("<h1>Hi Tushar </h1>")
+
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hi Tushar </h1>")
 })
-app.listen(process.env.PORT,()=>{
+
+app.get("/contact", (req, res) => {
+    res.send("Contact Page");
+})
+
+app.get("/service",(req,res)=>{
+res.send("<h2>This is the Service Page</h2>")
+})
+
+
+app.listen(process.env.PORT, () => {
     console.log("App server started at http://localhost:3000")
 })
